eb6d69844f63382d8e51b417eb2a640e3054c8ee
dhanraj mali <80768543+dhanraj644@users.noreply.github.com>
2026-07-25 00:29:31 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+        {
+            return false;
+        }
+        unordered_map<int,int>mp;
+        for(int i=0; i<s.size(); i++)
+        {
+            mp[s[i]]++;
+        }
+
+        for(auto x : t)
+        {
+            if(mp.find(x) != mp.end() && mp[x] >0)
+            {
+                mp[x]--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
