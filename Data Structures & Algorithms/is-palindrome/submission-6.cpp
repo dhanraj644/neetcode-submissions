ba5a61c4cbe163682950d494d32f8b54a7656f63
dhanraj mali <80768543+dhanraj644@users.noreply.github.com>
2026-08-01 11:37:25 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    private :
+        bool alphaNum(char c) {
+        return (c >= 'A' && c <= 'Z' ||
+                c >= 'a' && c <= 'z' ||
+                c >= '0' && c <= '9');
+    }
+    
+public:
+    bool isPalindrome(string s) {
+        
+        int start=0;
+        int end = s.size()-1;
+
+        while(start<end)
+        {
+           while(start <end && !alphaNum(s[start]))
+           {
+            start++;
+           }
+
+            while (end > start && !alphaNum(s[end])) {
+                end--;
+            }
+            if(tolower(s[start]) != tolower(s[end]))
+            {
+                cout<<s[start]<<s[end];
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+};
