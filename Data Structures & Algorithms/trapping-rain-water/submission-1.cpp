87ac073042f7028a0d4f94a0139f7858abedb02f
dhanraj mali <80768543+dhanraj644@users.noreply.github.com>
2026-08-05 17:46:15 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+        int n=height.size();
+        vector<int>prefixMax(n);
+        vector<int>suffixMax(n);
+
+        prefixMax[0]=height[0];
+
+        for(int i=1; i<n; i++)
+        {
+            if(height[i]>prefixMax[i-1])
+            {
+                prefixMax[i]=height[i];
+            }
+            else
+            {
+                prefixMax[i]=prefixMax[i-1];
+            }
+        }
+
+        suffixMax[n-1]=height[n-1];
+        for(int i=n-2; i>=0; i--)
+        {
+
+                if(height[i] >suffixMax[i+1] )
+                {
+                    suffixMax[i]=height[i];
+                }
+                else
+                {
+                    suffixMax[i]=suffixMax[i+1];
+                }
+        }
+
+        int total=0;
+        for(int i=0; i<n-1; i++)
+        {
+            if(height[i]<prefixMax[i] && height[i]<suffixMax[i])
+            {
+                total=total+(min(prefixMax[i],suffixMax[i])-height[i]);
+            }
+        }
+
+        return total;
+    }
+};
